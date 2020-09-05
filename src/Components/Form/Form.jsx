6e40147edc245174a274eeefdd96b7d2533bcc9f
fauziapriana29 <fauzicoding@gmail.com>
2020-09-05 16:09:36 +0700

    lATIHAN 14

---
+++ src/Components/Form/Form.jsx
@@ -0,0 +1,102 @@
+import React, {useState, useEffect} from 'react'
+import { useForm } from "react-hook-form";
+import axios from '../../../node_modules/axios/dist/axios.js'
+import Table from '../Table/Table.jsx'
+
+const Form = () => {
+    const [data, setData] = useState([]) 
+    const [update, setUpdate] = useState({})
+
+    useEffect(() => {
+        const getData = () => {
+            axios.get("http://localhost:3000/list").then((respone) => {
+                console.log(respone.data)
+                setData(respone.data)
+            }).catch((errors) => {
+              // console.log(errors)
+            });
+        }
+        getData()
+    }, [])
+
+     const { handleSubmit, register, errors } = useForm();
+    const onSubmit = (values, e) => {
+        axios.post("http://localhost:3000/list", values).then((respone) => {
+            // console.log('data masuk', respone.data)
+            setData([...data, respone.data])
+        }).catch((errors) => {
+            // console.log('post error')
+        });
+        e.target.reset()
+    };
+    
+    const onRemove = (id) => {
+        axios.delete(`http://localhost:3000/list/${id}`).then((respone) => {
+            const newData = data.filter((item) => {
+                if (item.id === id) return false
+                return true
+            })
+            setData(newData)
+        }).catch((error) => {
+            console.log(error)
+        });
+    }
+
+    const onUpdate = (id) => {
+        
+        const newDay = prompt('create new day')
+        const newActivites = prompt('create activites')
+
+        // setUpdate(Data)
+        axios.put(`http://localhost:3000/list/${id}`,{newDay,newActivites}).then((respone) => {
+            console.log('berhasil update')
+        }).catch((error) => {
+            console.log(error)
+        });
+            
+    } 
+    
+
+    return (
+      <div className="container">
+        <div className="form-group">
+          <br />
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <label htmlFor="list">Day:</label>
+            <input
+              type="text"
+              name="list"
+              className="form-control"
+              placeholder="Day"
+              ref={register({
+                required: "Required",
+              })}
+            />
+            {errors.list && errors.list.message}
+            <br />
+            <label htmlFor="list">Activites:</label>
+            <input
+              type="text"
+              name="Activites"
+              className="form-control"
+              placeholder="Activites"
+              ref={register({
+                required: "Required",
+              })}
+            />
+            {errors.list && errors.list.message}
+            <br />
+            <button type="submit" className="btn btn-success">
+              Submit
+            </button>
+          </form>
+        </div>
+        <br />
+            <Table todo={data} key={data.id} remove={onRemove} update={onUpdate}/>
+      </div>
+    );
+
+}
+
+export default Form
+
---
+++ src/Components/Table/Table.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+
+const Table = (props) => {
+
+    const { todo , remove, update} = props
+    const mapData = todo.map((data, index) => {
+        return (
+          <tr key={data.id}>
+            <th scope="row">{index + 1}</th>
+            <td>{data.list}</td>
+            <td>{data.Activites}</td>
+            <td>
+              <button className="btn btn-danger" onClick={() => remove(data.id)}>Delete</button> &nbsp;
+              <button className="btn btn-secondary" onClick={() => update(data.id)}>Edit</button>
+            </td>
+          </tr>
+        );
+    })
+    return (
+      <div className="container">
+        <table className="table table-striped">
+          <thead>
+            <tr>
+              <th scope="col">No.</th>
+              <th scope="col">Day</th>
+              <th scope="col">Avtivites</th>
+              <th scope="col">Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {mapData}
+          </tbody>
+        </table>
+      </div>
+    );
+}
+
+export default Table
